dd72fd6a578180d94847099faf88dc1c882ad6b5
Paula Leal <paula61395@gmail.com>
2021-12-01 18:59:24 -0500

    created client folder using vue 3 and created server folder with express etc.

---
+++ server/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const server = express();
+const cors = require('cors');
+
+server.use(express.json());
+server.use(cors());
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.dbURI, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to database!'));
+
+server.get("/", (req, res) => {
+    res.send("Hello World!");
+})
+
+server.listen(3000, () => console.log("Server started!"));
